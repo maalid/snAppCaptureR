4b4ff6e960d123714a6aa81af30afc417b1f9c4d
maalid <maalid@gmail.com>
2020-08-30 22:54:12 -0400

    first commit

--- dev/01_start.R
+++ dev/01_start.R
@@ -15,11 +15,11 @@
 ## Add meta data about your application
 golem::fill_desc(
   pkg_name = "snAppCaptureR", # The Name of the package containing the App 
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App 
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App 
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
+  pkg_title = "Take Snaps Using up to Two Webcams", # The Title of the package containing the App 
+  pkg_description = "A shiny app for take snapshots from one or two webcams. It allows to label the images and save them in the train-validation-test folder structure.", # The Description of the package containing the App 
+  author_first_name = "Marcelo", # Your First Name
+  author_last_name = "Alid-Vaccarezza", # Your Last Name
+  author_email = "maalid@gmail.com", # Your Email
   repo_url = NULL # The URL of the GitHub Repo (optional) 
 )     
 
@@ -28,7 +28,7 @@ golem::set_golem_options()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license( name = "Golem User" )  # You can set another license here
+usethis::use_mit_license( name = "Marcelo Alid" )  # You can set another license here
 usethis::use_readme_rmd( open = FALSE )
 usethis::use_code_of_conduct()
 usethis::use_lifecycle_badge( "Experimental" )
